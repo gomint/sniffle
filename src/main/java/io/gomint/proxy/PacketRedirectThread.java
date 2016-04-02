0ea2a6a436cbf7008fb777a88ccc7cd74818ce9d
Schuwi <schuwi4@gmail.com>
2016-04-03 00:37:34 +0200

    Added copyright header

--- src/main/java/io/gomint/proxy/PacketRedirectThread.java
+++ src/main/java/io/gomint/proxy/PacketRedirectThread.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2016, GoMint, BlackyPaw and geNAZt
+ *
+ * This code is licensed under the BSD license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package io.gomint.proxy;
 
 import io.gomint.jraknet.Connection;
